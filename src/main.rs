cc9fff91bb4d600cf675c75f97206a4a8d816032
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-05 14:56:18 +1300

    Comment borrow_mut calling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rust_smartpointers_refcell_cons_sample::List::{Cons, Nil};
-use std::cell::RefCell;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
     let value = Rc::new(RefCell::new(5));
@@ -9,6 +9,8 @@ fn main() {
     let b = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
     let c = Cons(Rc::new(RefCell::new(5)), Rc::clone(&a));
 
+    // Same as below
+    // *(*value).borrow_mut() += 10;
     *value.borrow_mut() += 10;
 
     println!("a: {:?}", a);
